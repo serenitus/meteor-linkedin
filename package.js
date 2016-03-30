e196e84119addef4403f8fe0fd325d6bbac7b680
Simon Turvey <simon.turvey@nokia.com>
2016-03-29 19:54:12 -0700

    Update package info for publication.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-    name: "pauli:linkedin",
-    summary: "A OAuth2 wrapper for the LinkedIN API",
-    version: "1.2.1",
-    git: "https://github.com/PauliBuccini/meteor-linkedin.git"
+    name: 'serenitus:linkedin',
+    summary: 'A OAuth2 wrapper for the LinkedIN API',
+    version: '1.2.1',
+    git: 'https://github.com/serenitus/meteor-linkedin.git'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('1.0.4')
+    api.versionsFrom('1.0.4');
 
     api.use('oauth2', ['client', 'server']);
     api.use('oauth', ['client', 'server']);
@@ -18,9 +18,7 @@ Package.onUse(function (api) {
 
     api.export('LinkedIn');
 
-    api.addFiles(
-        ['linkedin_configure.html', 'linkedin_configure.js'],
-        'client');
+    api.addFiles(['linkedin_configure.html', 'linkedin_configure.js'], 'client');
     api.addFiles('linkedin_common.js', ['client', 'server']);
     api.addFiles('linkedin_server.js', 'server');
     api.addFiles('linkedin_client.js', 'client');
